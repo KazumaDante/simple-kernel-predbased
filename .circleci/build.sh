f0e29ee8eeef21c6adbccedaed5e425bdb82b14f
KazuDante89 <karlemmanueldorvil@gmail.com>
2020-08-23 22:57:48 -0400

    fix-up 	modified:   .circleci/build.sh 	modified:   .circleci/config.yml

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,20 +1,20 @@
+
+
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone --depth=1 https://github.com/stormbreaker-project/android_kernel_xiaomi_lavender -b  rebase4lyf  kernel
+git clone --depth=1 https://github.com/KazuDante89/simple-kernel-predbased -b EAS3 kernel
 cd kernel
-git clone https://github.com/arter97/arm64-gcc --depth=1
-git clone https://github.com/arter97/arm32-gcc --depth=1
-git clone --depth=1 https://github.com/sohamxda7/AnyKernel3 AnyKernel
+git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
+git clone --depth=1 https://github.com/KazuDante89/AnyKernel3-EAS AnyKernel
 echo "Done"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 TANGGAL=$(date +"%F-%S")
 START=$(date +"%s")
 export CONFIG_PATH=$PWD/arch/arm64/configs/lavender-perf_defconfig
-PATH="$(pwd)/arm64-gcc/bin:$(pwd)/arm32-gcc/bin:${PATH}" \
+PATH="${PWD}/clang/bin:$PATH"
 export ARCH=arm64
-export USE_CCACHE=1
 export KBUILD_BUILD_HOST=circleci
-export KBUILD_BUILD_USER="sohamsen"
+export KBUILD_BUILD_USER="kazudante"
 # sticker plox
 function sticker() {
     curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
@@ -27,7 +27,7 @@ function sendinfo() {
         -d chat_id="$chat_id" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
-        -d text="<b>• Predator-Stormbreaker Kernel •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Xiaomi Redmi Note7/7S</b> (lavender)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b>#Stable"
+        -d text="<b>• [EAS]-Simple-Kernel-beta •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Xiaomi Redmi Note7</b> (lavender)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b>#beta"
 }
 # Push kernel to channel
 function push() {
@@ -51,18 +51,18 @@ function finerr() {
 # Compile plox
 function compile() {
    make O=out ARCH=arm64 lavender-perf_defconfig
-     PATH="$(pwd)/arm64-gcc/bin:$(pwd)/arm32-gcc/bin:${PATH}" \
        make -j$(nproc --all) O=out \
                              ARCH=arm64 \
-                             CROSS_COMPILE=aarch64-elf- \
-                             CROSS_COMPILE_ARM32=arm-eabi-
+                             CC=clang \
+			                       CROSS_COMPILE=aarch64-linux-gnu- \
+			                       CROSS_COMPILE_ARM32=arm-linux-gnueabi-
    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 }
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 Predator-Stormbreaker-lavender-${TANGGAL}.zip *
-    cd .. 
+    zip -r9 Simple-Kernel-beta-${TANGGAL}.zip *
+    cd ..
 }
 sticker
 sendinfo
@@ -71,4 +71,3 @@ zipping
 END=$(date +"%s")
 DIFF=$(($END - $START))
 push
-
